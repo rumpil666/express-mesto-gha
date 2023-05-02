55b2eca5c066de27fc8f4248f2c868433d71c439
Anton Mashkov <rumpil666@gmail.com>
2023-05-02 22:07:01 +0400

    1

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const errorHandler = require('./middlewares/errorHandler');
-const auth = require('./middlewares/auth');
 const { userRouter, cardRouter } = require('./routes');
 const NotFoundError = require('./errors/NotFoundError');
 const { login, createUser } = require('./controllers/users');
@@ -19,8 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 app.post('/signin', validationLogin, login);
 app.post('/signup', validationCreateUser, createUser);
 
-app.use('/users', auth, userRouter);
-app.use('/cards', auth, cardRouter);
+app.use('/users', userRouter);
+app.use('/cards', cardRouter);
 
 app.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
