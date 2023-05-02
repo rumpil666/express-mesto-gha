bb909100278f8d760e28d973422e0924b22761c3
Anton Mashkov <rumpil666@gmail.com>
2023-05-02 22:59:41 +0400

    1

--- models/user.js
+++ models/user.js
@@ -42,19 +42,22 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// eslint-disable-next-line func-names
-userSchema.statics.findUserByCredentials = (email, password) => this.findOne({ email })
-  .then((user) => {
-    if (!user) {
-      throw new UnauthorizedError('Неправильная почта или пароль');
-    }
-    return bcrypt.compare(password, user.password)
-      .then((matched) => {
-        if (!matched) {
-          throw new UnauthorizedError('Неправильная почта или пароль');
-        }
-        return user;
-      });
-  });
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError('Неправильная почта или пароль');
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new UnauthorizedError('Неправильная почта или пароль');
+          }
+
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
