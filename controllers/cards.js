f68dcec4445bc7aae60fff76cbe121f2c40567a7
Anton Mashkov <rumpil666@gmail.com>
2023-05-03 19:13:59 +0400

    BadRequest

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,7 +1,6 @@
 const httpConstants = require('http2').constants;
 const Card = require('../models/card');
 const NotFoundError = require('../errors/NotFoundError');
-const BadRequestError = require('../errors/BadRequestError');
 const ForbiddenError = require('../errors/ForbiddenError');
 
 module.exports.getCards = (req, res, next) => {
@@ -16,13 +15,7 @@ module.exports.createCard = (req, res, next) => {
 
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.status(httpConstants.HTTP_STATUS_CREATED).send(card))
-    .catch((e) => {
-      if (e.name === 'ValidationError') {
-        next(new BadRequestError('Переданы некорректные данные при создании карточки'));
-      } else {
-        next(e);
-      }
-    });
+    .catch(next);
 };
 
 module.exports.deleteCard = (req, res, next) => {
@@ -39,13 +32,7 @@ module.exports.deleteCard = (req, res, next) => {
           .catch(next);
       }
     })
-    .catch((e) => {
-      if (e.name === 'CastError') {
-        next(new BadRequestError('Некорректный ID карточки'));
-      } else {
-        next(e);
-      }
-    });
+    .catch(next);
 };
 
 module.exports.likeCard = (req, res, next) => {
@@ -61,13 +48,7 @@ module.exports.likeCard = (req, res, next) => {
         res.send(card);
       }
     })
-    .catch((e) => {
-      if (e.name === 'CastError') {
-        next(new BadRequestError('Некорректный ID карточки'));
-      } else {
-        next(e);
-      }
-    });
+    .catch(next);
 };
 
 module.exports.dislikeCard = (req, res, next) => {
@@ -83,11 +64,5 @@ module.exports.dislikeCard = (req, res, next) => {
         res.send(card);
       }
     })
-    .catch((e) => {
-      if (e.name === 'CastError') {
-        next(new BadRequestError('Некорректный ID карточки'));
-      } else {
-        next(e);
-      }
-    });
+    .catch(next);
 };
