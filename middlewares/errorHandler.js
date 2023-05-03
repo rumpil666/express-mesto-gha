765488fb20620c9fa0d35c7e44f8e73f9baa676b
Anton Mashkov <rumpil666@gmail.com>
2023-05-03 18:36:35 +0400

    h

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,11 +1,13 @@
-const httpConstants = require('http2').constants;
+// const httpConstants = require('http2').constants;
 
 const errorHandler = (err, req, res, next) => {
-  const status = err.statusCode || httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
+  const status = err.statusCode;
   console.log(err.stack || err);
+  const { message } = err;
 
   res.status(status).send({
     err,
+    message,
   });
   next();
 };
